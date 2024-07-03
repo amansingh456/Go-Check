1cedae0826eab6628b6c332a2e4703262cef8ea8
amansinghrajawat <aman@onmeta.in>
2024-07-03 22:58:28 +0530

    today's task finished

--- pointersElevan/service.go
+++ pointersElevan/service.go
@@ -2,6 +2,29 @@ package pointerselevan
 
 import "fmt"
 
+func updateValueByRef(a *int) {
+	*a = *a + 105
+}
+
 func Sample() {
 	fmt.Println("Hello from pointers")
+
+	//!
+	// var num int
+	// num = 2
+
+	// var ptr *int
+	// ptr = &num
+
+	num := 2
+	ptr := &num
+
+	fmt.Println(num)
+	fmt.Println("what ptr have", ptr)      // this is the only address
+	fmt.Println("what ptr contains", *ptr) // if you want to use value of ptr then use astric(*)
+
+	//! if want to update original value through pointer address then-
+	janam := 101
+	updateValueByRef(&janam)                  // remember we only passed address in this func
+	fmt.Println("changed janam value", janam) //? 101 to 206
 }
